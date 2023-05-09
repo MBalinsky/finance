1d4189314e07343b8030c3fc9feddbadad647f2e
MBalinsky <supermike820@gmail.com>
2023-05-10 00:26:04 +0300

    feat: add adapive

---
+++ js/main.js
@@ -0,0 +1,91 @@
+//burger-menu
+const burger = document.querySelector('.menu-wrapper');
+const menu = document.querySelector('header nav ul')
+
+if(burger){
+    burger.addEventListener('click', () => {
+        document.querySelector('.hamburger-menu').classList.toggle('animate');
+        menu.classList.toggle('menu-active');
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const slide = Array.from(document.querySelectorAll('.swiper-slide'))
+let swiper
+
+if(slide.length < 3){
+    swiper = new Swiper('.swiper', {
+        slidesPerView: 'auto',
+        breakpoints:{
+            1000:
+            {spaceBetween: 164,}
+        },
+        centeredSlides: true,
+        grabCursor: true,
+        longSwipes: false,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        }
+    })
+}
+else if(slide.length === 3){
+    swiper = new Swiper('.swiper', {
+        slidesPerView: 'auto',
+        breakpoints:{
+            1000:
+            {spaceBetween: 164,}
+        },
+        centeredSlides: true,
+        loop: true, 
+        loopedSlides: 1,
+        grabCursor: true,
+        longSwipes: false,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        }
+    })
+}
+else if (slide.length > 3){
+    swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        slidesPerView: 'auto',
+        breakpoints:{
+            1000:
+            {spaceBetween: 164,}
+        },
+        centeredSlides: true,
+        loop: true, 
+        loopedSlides: 2,
+        grabCursor: true,
+        longSwipes: false,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
+    })
+}
